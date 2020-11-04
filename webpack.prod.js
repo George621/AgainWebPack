9683d3b0b142560349167d9d10f1bb2fdd221658
zhangjunshuai <junshuai.zhang@fenbeitong.com>
2020-11-04 23:57:34 +0800

    多页面打包 动态获取entry HtmlWebpackPlugins

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,3 +1,4 @@
+const glob = require('glob');
 const path = require('path');
 const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -6,12 +7,43 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 // const autoprefixer = require('autoprefixer');
 
+const setMAP = () => { // 动态获取多入口打包 entry  HtmlWebpackPlugins
+  const entry = {};
+  const HtmlWebpackPlugins = [];
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'));
+
+  Object.keys(entryFiles).map((index)=>{
+    const entryFile = entryFiles[index];
+    // /Users/georgezhang/Desktop/AgainWebPack/src/index/index.js
+    const  match = entryFile.match(/src\/(.*)\/index.js/);
+    const pageName = match && match[1];
+
+    entry[pageName] = entryFile;
+    HtmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false
+        }
+      }),
+    )
+  })
+  return { HtmlWebpackPlugins, entry }
+}
+
+const  { HtmlWebpackPlugins, entry } = setMAP();
+
 module.exports = {
   // entry: './src/index.js', // 单入口
-  entry: {
-    index: './src/index/index.js',
-    search: './src/search/search.js'
-  },
+  entry:  entry,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name]_[chunkhash:8].js'
@@ -52,7 +84,7 @@ module.exports = {
             loader: 'px2rem-loader',
             options: {
               remUnit: 75,
-              remPrecision: 8 
+              remPrecision: 8
             }
           }
         ]
@@ -91,34 +123,7 @@ module.exports = {
     new OptimizeCssAssetsWebpackPlugin({
       assetNameRegExp: /\.css$/g,
       cssProcessor: require('cssnano')
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/search/search.html'),
-      filename: 'search.html',
-      chunks: 'search',
-      inject: true,
-      minify: {
-        html5: true,
-        collapseWhitespace: true,
-        preserveLineBreaks: false,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false
-      }
-    }),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src/index/index.html'),
-      filename: 'index.html',
-      chunks: 'index',
-      inject: true,
-      minify: {
-        html5: true,
-        collapseWhitespace: true,
-        preserveLineBreaks: false,
-        minifyCSS: true,
-        minifyJS: true,
-        removeComments: false
-      }
-    }),
-  ]
+    })
+    
+  ].concat(HtmlWebpackPlugins)
 };
