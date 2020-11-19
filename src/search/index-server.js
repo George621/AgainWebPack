6b5167fd539301d042b2707f943faa698dbc8246
zhangjunshuai <junshuai.zhang@fenbeitong.com>
2020-11-20 00:23:11 +0800

    ssr learn half

---
+++ src/search/index-server.js
@@ -0,0 +1,32 @@
+import React, { PureComponent } from 'react'
+import ReactDom from 'react-dom'
+import './search.less'
+import logo from './images/invoice_has_check.svg'
+
+class Search extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      Text: null
+    }
+  }
+
+  loadComponent (){
+    import('./text.js').then((data) => {
+      console.log(data)
+      this.setState({
+        Text: data
+      })
+    });
+  }
+
+  render() {
+    const { Text } = this.state
+    return <div className='search-text'>
+      {Text ? <Text /> : ''}
+      <img src={logo} onClick={this.loadComponent.bind(this)} alt="" />
+    </div>
+  }
+}
+
+module.exports = Search ;
---
+++ webpack.ssr.js
@@ -0,0 +1,143 @@
+const glob = require('glob');
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin');
+// const autoprefixer = require('autoprefixer');
+
+const setMAP = () => { // 动态获取多入口打包 entry  HtmlWebpackPlugins
+  const entry = {};
+  const HtmlWebpackPlugins = [];
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'));
+
+  Object.keys(entryFiles).map((index) => {
+    const entryFile = entryFiles[index];
+    // /Users/georgezhang/Desktop/AgainWebPack/src/index/index.js
+    const match = entryFile.match(/src\/(.*)\/index.js/);
+    const pageName = match && match[1];
+
+    entry[pageName] = entryFile;
+    HtmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: ['vendors',pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false
+        }
+      }),
+    )
+  })
+  return { HtmlWebpackPlugins, entry }
+}
+
+const { HtmlWebpackPlugins, entry } = setMAP();
+
+module.exports = {
+  // entry: './src/index.js', // 单入口
+  entry: entry,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]-server.js',
+    libraryTarget: 'umd'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /.css$/,
+        use: [
+          // 'style-loader', // MiniCssExtractPlugin 插件冲突，一个是提出一个文件，另一个是插入head标签中
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          // 'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')()
+                ]
+              }
+            }
+          },
+          {
+            loader: 'px2rem-loader',
+            options: {
+              remUnit: 75,
+              remPrecision: 8
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              limit: 10240,   // 小于这个数字则打包成base 64
+              name: '[name]_[hash:8].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              limit: 10240,
+              name: '[name]_[hash:8][ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    }),
+    new OptimizeCssAssetsWebpackPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano')
+    }),
+    
+  ].concat(HtmlWebpackPlugins),
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /react|react-dom/,
+          name: 'vendors',
+          chunks: 'all',
+          minChunks: 1
+        }
+      }
+    }
+  }
+};
